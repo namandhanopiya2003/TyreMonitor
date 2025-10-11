8de6448e8a6457db9a462c09b6de17276a04bbbd
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-10-11 09:19:43 +0530

    Updated documentation

--- TyreMonitor_Android_App/kt_files/Vehicle.kt
+++ TyreMonitor_Android_App/kt_files/Vehicle.kt
@@ -1,9 +1,10 @@
 package com.example.tyremonitor
 
+// Data class that stores information about vehicle and its tyres
 data class Vehicle(
     val name: String,
     val regNo: String,
     val tyrePressure: Double,
     val tyreAge: String,
     val loadCapacity: String
-) : java.io.Serializable
+) : java.io.Serializable              // This enables to send this Vehicle object between screens (activities)
